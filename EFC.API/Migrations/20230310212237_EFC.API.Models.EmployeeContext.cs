4c0a7719f082d88f55729e0ac95695530a7b96ef
Elio Cortes <elio.cortes3000@gmail.com>
2023-03-10 16:55:56 -0500

    Setup Web.API and Tests projects

---
+++ EFC.API/Migrations/20230310212237_EFC.API.Models.EmployeeContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFC.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class EFCAPIModelsEmployeeContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ EFC.API/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFC.API.Models
+{
+    public class EmployeeContext: DbContext
+    {
+
+        public EmployeeContext(DbContextOptions options)
+           : base (options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
